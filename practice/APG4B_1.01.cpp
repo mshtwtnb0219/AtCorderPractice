4bd600665bac20ae15a34dc09dc8a86cc65b11b1
mshtwtnb0219 <b77yellow@gmail.com>
2024-09-15 15:45:39 +0900

    apg4b開始

---
+++ practice/APG4B_1.01.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // 文字列を出力する
+    // endlは改行
+    cout << "hellow" << endl;
+    cout << "あああ";
+    // 数値を出力する
+    cout << 11123;
+
+
+
+    // 問題
+    cout <<"こんにちは" << endl;
+    cout <<"Atcoder";
+}
---
+++ practice/APG4B_1.03.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << 2 + 3 << endl;
+    cout << 2 - 3 << endl;
+    cout << 2 * 3 << endl;
+    cout << 6 / 3 << endl;
+
+
+    // 問題
+    cout << 0.5 * 100 * (100 + 1) ;
+
+}
